92a073979e85f2ae092827d2d27d7cf0a81a1caa
runawaysheepkd <runawaysheep+kd@gmail.com>
2025-12-24 16:36:07 +0900

    개발용 프록시 설정

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'standalone'
+  output: 'standalone',
+  
+  // Rewrites 설정 추가
+  async rewrites() {
+    return [
+      {
+        // 개발 환경용 프록시 설정
+        source: '/api/:path*',
+        destination: 'http://10.125.121.183:9090/api/:path*', 
+      },
+    ];
+  },
 };
 
 export default nextConfig;
