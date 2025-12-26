47539ba8e8d590b13d41a655c43ce6f121bd7b6f
cy979 <ceyeye33@gmail.com>
2025-12-26 14:26:19 +0900

    dashboard

--- src/components/ContainerCard.tsx
+++ src/components/ContainerCard.tsx
@@ -1,7 +1,7 @@
 
 export default function Page() {
   return (
-    <div className="w-full h-full p-4 shadow-xl bg-gray-100 text-lg text-neutral-700 rounded-xl 
+    <div className="w-full h-full p-4 shadow-xl bg-gray-50 text-lg text-neutral-700 rounded-2xl 
                     items-center justify-center">
       지표별 수치 카드
     </div>
