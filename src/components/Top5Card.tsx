02d4f1e38f395b3e03f0c63a23d41bcacd715d8a
cy979 <229726138+cy979@users.noreply.github.com>
2026-01-05 09:23:04 +0900

    Top5

--- src/components/Top5Card.tsx
+++ src/components/Top5Card.tsx
@@ -1,9 +1,17 @@
+import type {ChartDataItem} from '@/types/dashboard'
 
-export default function Page() {
+interface Top5CardProps {
+  item: ChartDataItem[],
+  caption: string
+}
+
+export default function Top5Card({item, caption}:Top5CardProps) {
+  
   return (
     <div className="w-full p-4 shadow-xl bg-gray-50 text-lg text-neutral-700 rounded-2xl 
                     items-center justify-center">
-      (행정구역별)TOP5
+                      <div className="text-neutral-700 font-bold">{caption}</div>
+                      <div className="text-neutral-700 font-bold">{item.values}</div>
     </div>
   );
 }
