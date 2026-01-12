c85fa646b2a833ebf41986a711c2f312c2af1921
runawaysheepkd <runawaysheep+kd@gmail.com>
2026-01-12 14:25:12 +0900

    가이드 수정

--- src/app/map/Guide.tsx
+++ src/app/map/Guide.tsx
@@ -19,27 +19,27 @@ export default function Guide({}) {
         { showCurrent && 
             <div className='w-full h-screen inset-0 bg-black/50 flex items-center justify-between absolute top-0 left-0 z-100 text-white'>
                 <div className="w-1/3 h-full flex">
-                    <div className="w-80 h-full bg-green-500/50">
+                    <div className="w-80 bg-green-500/50">
                         영역1
                     </div>
-                    <div className="flex-1 h-full bg-yellow-500/50">
+                    <div className="flex-1 bg-yellow-500/50">
                         영역2
                     </div>
                 </div>
                 <div className='w-1/3 h-full flex flex-col justify-center'>
-                    <div className='w-full flex-1 bg-red-500/50'>
+                    <div className='flex-1 bg-red-500/50'>
                         영역3
                     </div>
-                    <div className='w-full h-20 py-5 flex justify-center'>
+                    <div className='h-20 py-5 flex justify-center'>
                         <button className="bg-sky-600 text-white px-3 py-2 rounded-md cursor-pointer mr-2" onClick={()=>close(true)} >다시보지 않기</button>
                         <button className="bg-sky-600 text-white px-3 py-2 rounded-md cursor-pointer" onClick={()=>close()} >닫기</button>
                     </div>
                 </div>
                 <div className="w-1/3 h-full flex flex-col">
-                    <div className="w-full h-1/2 bg-green-500/50">
+                    <div className="h-1/2 bg-green-500/50">
                         영역4
                     </div>
-                    <div className="flex-1 h-1/2 bg-yellow-500/50">
+                    <div className="h-1/2 bg-yellow-500/50">
                         영역5
                     </div>
                 </div>
